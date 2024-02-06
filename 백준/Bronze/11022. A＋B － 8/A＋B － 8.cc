2834df16792189efb5b5351bd5baa36be0fd8de3
soyoungxxx <100336643+soyoungxxx@users.noreply.github.com>
2024-02-06 18:35:39 +0900

    [Bronze V] Title: A+B - 8, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int T;
+
+    cin >> T;
+
+    for (int i = 1; i <= T; i++) {
+        int A, B;
+        cin >> A >> B;
+        printf("Case #%d: %d + %d = %d\n", i, A, B, A + B);
+    }
+    return 0;
+}
