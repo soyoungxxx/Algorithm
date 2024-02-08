51b6d85b9028c36f0f4e59e773a1650e6a01a732
soyoungxxx <100336643+soyoungxxx@users.noreply.github.com>
2024-02-08 23:15:40 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < i; j++) {
+            printf(" ");
+        }
+        for (int j = N - i; j > 0; j--) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
