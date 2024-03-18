1c21bd9579a8b86dedf4332a704bbcdfc4552f37
soyoungxxx <100336643+soyoungxxx@users.noreply.github.com>
2024-03-18 09:48:20 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[end_num - start_num + 1];
+        int num = start_num;
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = num;
+            num += 1;
+        }
+        return answer;
+    }
+}
