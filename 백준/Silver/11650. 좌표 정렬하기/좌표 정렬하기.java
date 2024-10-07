65ece0efb99610666ec1bc7abb6e8a43709b2887
soyoungxxx <100336643+soyoungxxx@users.noreply.github.com>
2024-10-07 15:18:04 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 748 ms, Memory: 57532 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            String[] inputs = br.readLine().split(" ");
+            arr[i][0] = Integer.parseInt(inputs[0]);
+            arr[i][1] = Integer.parseInt(inputs[1]);
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] == o2[0]) return o1[1] - o2[1];
+                return o1[0] - o2[0];
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            bw.write(arr[i][0] + " " + arr[i][1] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
