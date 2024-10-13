acc137619c2493e6fbc6c6299eb6b84529fe681e
soyoungxxx <100336643+soyoungxxx@users.noreply.github.com>
2024-10-14 05:59:46 +0900

    [Silver I] Title: 회의실 배정, Time: 468 ms, Memory: 44092 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] times = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            times[i][0] = Integer.parseInt(st.nextToken());
+            times[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(times, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                }
+                return o1[0] - o2[0];
+            }
+        });
+
+        int start = times[0][0], end = times[0][1];
+        int answer = 1;
+        for (int i = 1; i < N; i++) {
+            int compareStart = times[i][0], compareEnd = times[i][1];
+            if (compareStart < end && compareEnd < end) {
+                start = compareStart;
+                end = compareEnd;
+                continue;
+            }
+            if (compareStart < end) {
+                continue;
+            }
+            answer += 1;
+            start = compareStart;
+            end = compareEnd;
+        }
+        System.out.println(answer);
+    }
+}
