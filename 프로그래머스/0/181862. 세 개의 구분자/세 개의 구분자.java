07aacad136e4e1f640c74ecfcd2a656e9b11332f
soyoungxxx <100336643+soyoungxxx@users.noreply.github.com>
2025-10-11 18:05:16 +0900

    [level 0] Title: 세 개의 구분자, Time: 45.70 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String myStr) {
+        String[] split = myStr.split("[abc]");
+
+        List<String> result = new ArrayList<>();
+        for (String s : split) {
+            if (!s.isEmpty()) {
+                result.add(s);
+            }
+        }
+
+        if (result.isEmpty()) {
+            return new String[] {"EMPTY"};
+        }
+
+        return result.toArray(new String[0]);
+    }
+}
