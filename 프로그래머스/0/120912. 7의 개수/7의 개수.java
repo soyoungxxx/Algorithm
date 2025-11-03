f0d518fdf94932d903dd14bd08af0407d3b2c972
soyoungxxx <100336643+soyoungxxx@users.noreply.github.com>
2025-11-04 00:16:28 +0900

    [level 0] Title: 7의 개수, Time: 0.03 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        for (int num: array) {
+            while (num > 0) {
+                if (num % 10 == 7) answer += 1;
+                num /= 10;
+            }
+        }
+        return answer;
+    }
+}
