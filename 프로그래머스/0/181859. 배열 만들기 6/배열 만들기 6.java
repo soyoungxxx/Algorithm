a270e0e8f6c4eeb9af8480a2366352759dedf6a6
soyoungxxx <100336643+soyoungxxx@users.noreply.github.com>
2025-11-03 23:59:42 +0900

    [level 0] Title: 배열 만들기 6, Time: 53.21 ms, Memory: 125 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int[] arr) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (answer.size() == 0) 
+                answer.add(arr[i]);
+            else if (answer.get(answer.size()-1) == arr[i]) 
+                answer.remove(answer.size()-1);
+            else
+                answer.add(arr[i]);
+        }
+        if(answer.size() == 0) answer.add(-1);
+        return answer;
+    }
+}
